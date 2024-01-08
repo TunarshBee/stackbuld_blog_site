81b8e5616ba9b5d3e3187f653774ffb1f803fdd0
TunarshBee <tunarsh4real@gmail.com>
2024-01-08 15:22:07 +0100

    Getting a single post

--- src/app/modules/post-details/post-details.component.ts
+++ src/app/modules/post-details/post-details.component.ts
@@ -1,23 +1,36 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { PostService } from '../../services/post.service';
+import { IPost } from '../../../store/models/post.models';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { Store } from '@ngrx/store';
+import { deleteBlogPost } from '../../../store/post.action';
 
 @Component({
   selector: 'app-post-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, RouterLink],
   templateUrl: './post-details.component.html',
   styleUrl: './post-details.component.scss'
 })
 export class PostDetailsComponent {
-  post: any = {};
+  post!: IPost
+  postId!: string
 
-  constructor(private route: ActivatedRoute, private postService: PostService) {}
+  constructor(private route: ActivatedRoute, private postService: PostService, private router: Router, private store: Store) { }
 
   ngOnInit() {
-    const postId = this.route.snapshot.paramMap.get('id') as string;
-    this.postService.getPostById(postId).subscribe(post => {
+    this.postId = this.route.snapshot.paramMap.get('id') as string;
+    this.postService.getPostById(this.postId).subscribe(post => {
       this.post = post;
     });
+  }
+
+  onDelete() {
+    if (confirm("Are you sure you want to delete this post")) {
+      this.store.dispatch(deleteBlogPost({ postId: this.postId }))
+      this.router.navigate(["/"])
+    }
   }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
+import { Store } from '@ngrx/store';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
   private apiUrl = environment.apiUrl;
-  
-  constructor(private http: HttpClient) {}
 
-  getPosts(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/post`);
+  constructor(private http: HttpClient, private store: Store) {
+  }
+
+  getPosts(page: number = 0): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/post?page=${page}`);
   }
 
   getPostById(id: string): Observable<any> {
@@ -24,13 +27,11 @@ export class PostService {
     return this.http.post<any>(`${this.apiUrl}/post/create`, post);
   }
 
-  updatePost(post: any): Observable<any> {
-
+  updatePost(id: string, post: any): Observable<any> {
     return this.http.put<any>(`${this.apiUrl}/post/${post.id}`, post);
   }
 
   deletePost(id: string): Observable<any> {
-
     return this.http.delete<any>(`${this.apiUrl}/post/${id}`);
   }
 }
