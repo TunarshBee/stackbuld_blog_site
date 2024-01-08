69ac7d64d0de4ba19a7314a8fb8a2aad33b53f44
TunarshBee <tunarsh4real@gmail.com>
2024-01-08 15:21:19 +0100

    Editing a post

--- src/app/modules/edit-post/edit-post.component.ts
+++ src/app/modules/edit-post/edit-post.component.ts
@@ -1,28 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostService } from '../../services/post.service';
-import { Router } from '@angular/router';
-import { FormControl, FormGroup } from '@angular/forms';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IPost } from '../../../store/models/post.models';
+import { Store } from '@ngrx/store';
+import { updateBlogPost } from '../../../store/post.action';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 @Component({
   selector: 'app-edit-post',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,MatFormFieldModule,MatButtonModule, MatInputModule],
   templateUrl: './edit-post.component.html',
   styleUrl: './edit-post.component.scss'
 })
-export class EditPostComponent {
-  // post: any = { title: '', content: '' };
-  editPostForm = new FormGroup({
-    title: new FormControl(""),
-    content: new FormControl("")
+export class EditPostComponent implements OnInit {
+  postId!: string;
+  post!: IPost;
+  isLoading!: boolean
+
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute,
+    private postService: PostService,
+  ) {
+    this.postId = this.route.snapshot.paramMap.get('id') as string;
+    this.loadPost();
+   }
+
+  ngOnInit() {
+    
+  }
+
+  editForm = new FormGroup({
+    text: new FormControl("", Validators.required),
+    likes: new FormControl("", Validators.required),
+    tags: new FormControl("", Validators.required)
   })
 
-  constructor(private postService: PostService, private router: Router) {}
+  loadPost() {
+    this.postService.getPostById(this.postId).subscribe((post: IPost) => {
+      this.post = post;
+      let tags = ""
+      post.tags?.forEach(element => {
+        tags += element
+      });
+      this.editForm.setValue({
+        likes: post.likes as string,
+        tags: tags,
+        text: post.text as string
+      })
+    });
+
+  }
 
   savePost() {
-    const {content, title} = this.editPostForm.value
-    this.postService.createPost({content,title}).subscribe(() => {
-      this.router.navigate(['/']);
-    });
+    const { likes, tags, text } = this.editForm.value
+    let tagArray = tags?.split(" ")
+    this.store.dispatch(updateBlogPost({ post: { id: this.postId, likes: likes as string, tags: tagArray, text: text as string } }))
+    this.router.navigate(['/post', this.postId]);
   }
 }
