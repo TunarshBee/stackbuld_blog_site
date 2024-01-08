f567cda53bd4b3e10bbd8148131212d27082468e
TunarshBee <tunarsh4real@gmail.com>
2024-01-08 15:20:28 +0100

    added routing and http config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,13 +7,28 @@ import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@a
 import { ApiKeyInterceptor } from './services/injector.service';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { blogReducer } from '../store/post.reducer';
+import { BlogEffects } from '../store/post.effects';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(withHttpTransferCacheOptions({
+    providers: [
+    provideRouter(routes),
+    provideClientHydration(withHttpTransferCacheOptions({
         includePostRequests: true
-    })), provideHttpClient(withInterceptorsFromDi()), {
+    })),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
         provide: HTTP_INTERCEPTORS,
         useClass: ApiKeyInterceptor,
         multi: true
-    }, provideStore(), provideEffects()]
+    },
+    provideStore({
+        blog: blogReducer
+    }),
+    provideEffects([
+        BlogEffects
+    ]),
+    provideAnimations()
+]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,22 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { LandingPageComponent } from './modules/landing-page/landing-page.component';
+import { PostDetailsComponent } from './modules/post-details/post-details.component';
+import { EditPostComponent } from './modules/edit-post/edit-post.component';
+import { CreatePostComponent } from './modules/create-post/create-post.component';
 
 export const routes: Routes = [
     {
         path:"",
         component: NavbarComponent,
-        children:[{
+        children:[
+            {
             path:"",
             component:LandingPageComponent
-        }]
+        },
+        { path: 'post/new', component: CreatePostComponent },
+        { path: 'post/:id', component: PostDetailsComponent },
+        { path: 'post/:id/edit', component: EditPostComponent },
+    ]
     }
 ];
