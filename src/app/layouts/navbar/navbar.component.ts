66a7e8b4829446a2c6c6b28ac119d36c0083e07a
TunarshBee <tunarsh4real@gmail.com>
2024-01-08 15:20:42 +0100

    Navbar

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet,],
+  imports: [RouterOutlet,MatToolbarModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
